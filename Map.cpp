6940e5ee8c430a094828fafd1057b8308db268c1
KonoLucas <158689821+KonoLucas@users.noreply.github.com>
2024-09-24 01:20:43 +0000

    Implement of territory Class

--- Map.cpp
+++ Map.cpp
@@ -1,2 +1,73 @@
 #include "Map.h"
 #include <queue>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+class Territory {
+    private:
+    int x,y;
+    int armies;
+    std::string name;
+    std::string owner;
+    std::string continent;
+    std::vector<std::string> adjacentTerritoryNames;
+    std::vector<Territory*> adjacentTerritories;
+
+
+    public:
+
+    Territory::Territory(std::string name, int x, int y, std::string continent, const std::vector<std::string>& adjNames) : name(name), x(x), y(y), continent(continent), adjacentTerritoryNames(adjNames) {}
+    //Constructor(Name + X + Y + Continent + vertor (adjacentTerritoryNames) );
+
+    Territory::~Territory() {}
+
+    void setOwner(const std::string& newOwner){owner=newOwner;};
+    void setArmies(int numArmies){armies=numArmies;};
+    void Territory::addAdjacentTerritory(Territory* territory) {adjacentTerritories.push_back(territory);}
+    //Set method;
+
+    int getArmies() const {return armies;};
+    std::string getName() const {return name;};
+    std::string getOwner() const {return owner;};
+    std::string getContinent() const {return continent;};
+    std::vector<std::string> getAdjacentTerritoryNames() const {return adjacentTerritoryNames;};
+    std::vector<Territory*> getAdjacentTerritories() const {return adjacentTerritories;};
+    //Get method;
+
+    void Territory::printTerritoryInfo() const {
+    std::cout << "Territory: " << name << " (" << x << ", " << y << ") in " << continent
+              << ". Adjacent territories: ";
+    for (Territory* adj : adjacentTerritories) {
+        std::cout << adj->getName() << " ";
+    }
+    std::cout << std::endl;
+}
+
+};
+
+
+
+class Map {
+private:
+    std::string author;
+    std::string warn;
+    std::string image;
+    std::string wrap;
+    std::string horizontal;
+
+    std::vector<Territory*> territories;
+    std::vector<Continent*> continents;
+
+    bool isConnectedGraph() const;
+    bool areContinentsConnected() const;
+
+public:
+    Map();
+    ~Map();
+
+    void addTerritory(Territory* territory);
+    void addContinent(Continent* continent);
+
+    bool validate() const;
+};
--- Map.h
+++ Map.h
@@ -1,8 +1,83 @@
 #include <string>
 #include <vector>
 
-class Territory {};
+class Territory {
+    private:
+    int x,y;
+    int armies;
+    std::string name;
+    std::string owner;
+    std::string continent;
+    std::vector<std::string> adjacent_TerritoryNames;
+    std::vector<Territory*> adjacent_Territories;
 
 
-class Continent {};
+    public:
 
+    Territory(std::string name, int x, int y, std::string continent, const std::vector<std::string>& adjNames); //Constructor(Name + X + Y + Continent + vertor (adjacentTerritoryNames) );
+    ~Territory();
+
+    void setOwner(const std::string& owner);
+    void setArmies(int numArmies);
+    void addAdjacentTerritory(Territory* territory);
+    //Set functions;
+
+    int getArmies() const;
+    std::string getName() const;
+    std::string getOwner() const;
+    std::string getContinent() const;
+    std::vector<std::string> getAdjacentTerritoryNames() const;
+    std::vector<Territory*> getAdjacentTerritories() const;
+    //Get functions;
+
+    void printTerritoryInfo() const;  // print function
+
+};
+
+
+class Continent {
+    private:
+    std::string name;
+    int controlValue;
+    std::vector<std::string> continents_TerritoryNames;
+    std::vector<Territory*> continents_Territory;
+    
+
+    public:
+    Continent(std::string name, int controlValue);//Constructor(Name + controlValue );
+    ~Continent();
+    
+    void addTerritory(Territory* territory);
+
+    std::vector<Territory*> getTerritories() const;
+    std::string getName() const;
+    int getControlValue() const;
+    //Get functions;
+
+    void printContinentInfo() const;  // print function
+
+};
+
+class Map {
+private:
+    std::string author;
+    std::string warn;
+    std::string image;
+    std::string wrap;
+    std::string horizontal;
+
+    std::vector<Territory*> territories;
+    std::vector<Continent*> continents;
+
+    bool isConnectedGraph() const;
+    bool areContinentsConnected() const;
+
+public:
+    Map();
+    ~Map();
+
+    void addTerritory(Territory* territory);
+    void addContinent(Continent* continent);
+
+    bool validate() const;
+};
