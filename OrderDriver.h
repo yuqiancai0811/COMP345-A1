252e1e0aae3ce0da86421a800c1658017048468f
mengge-he <125909363+mengge-he@users.noreply.github.com>
2024-09-29 15:48:45 -0400

    Add files via upload

---
+++ OrderDriver.h
@@ -0,0 +1,16 @@
+//
+// Created by Crimson on 2024-09-29.
+//
+
+#ifndef ORDERDRIVER_H
+#define ORDERDRIVER_H
+
+
+
+class OrderDriver {
+
+};
+
+
+
+#endif //ORDERDRIVER_H
---
+++ Orders.h
@@ -0,0 +1,16 @@
+//
+// Created by Crimson on 2024-09-29.
+//
+
+#ifndef ORDERS_H
+#define ORDERS_H
+
+
+
+class Orders {
+
+};
+
+
+
+#endif //ORDERS_H
