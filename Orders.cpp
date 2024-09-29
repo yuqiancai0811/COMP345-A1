10f618523e9055235655bc59d7401abdd45faf19
mengge-he <125909363+mengge-he@users.noreply.github.com>
2024-09-29 16:55:39 -0400

    Update Orders.cpp

--- Orders.cpp
+++ Orders.cpp
@@ -9,8 +9,10 @@
 class Order {
     protected:
         std::string effect;
-        bool executed;
+        bool executed=false;
     public:
+        std::string name;
+
         Order() : executed(false) {}
         virtual ~Order() {}
 
@@ -18,16 +20,33 @@ class Order {
         virtual bool validate() const = 0;
         virtual void execute() = 0;
 
+        virtual std::string toString() const {
+           // return "Order: " + name + (executed ? " (Executed)" : " (Not executed)") + "\nEffect: " + effect + "\n";
+            if (executed&&!name.empty()) {
+                return "Order:"+ name + " is executed\n";
+            }
+            else if(!executed&&!name.empty()) {
+                return "Order:" + name + " is not executed\n";
+            }
+            else {
+                return"Order:Null\n";
+            }
+        }
     // Stream insertion operator
         friend std::ostream& operator<<(std::ostream& os, const Order& order) {
-            os << "Order: " << typeid(order).name() << (order.executed ? " (Executed)" : " (Not executed)") << "\n";
+            os << "Order: " << order.name << (order.executed ? " (Executed)" : " (Not executed)") << "\n";
             os << "Effect: " << order.effect << "\n";
             return os;
+
+
     }
 };
 
 class deployOrder : public Order {
   public:
+    deployOrder() {
+        name = "deployOrder";
+    }
     bool validate() const override {
         // For now, let's assume this order is always valid
         return true;
@@ -39,10 +58,18 @@ class deployOrder : public Order {
             executed = true;
         }
     }
+    std::string toString() const override {
+        return Order::toString(); // Use the base class implementation
+    }
 };
 
 class advanceOrder : public Order {
   public:
+
+    advanceOrder() {
+        name = "Advance Order";
+    }
+    std::string name="advanceOreder";
     bool validate() const override {
         // For now, let's assume this order is always valid
         return true;
@@ -54,10 +81,16 @@ class advanceOrder : public Order {
             executed = true;
         }
     }
+    std::string toString() const override {
+        return Order::toString(); // Use the base class implementation
+    }
 };
 
 class bombOrder : public Order {
   public:
+    bombOrder() {
+        name="Bomb Order";
+    }
     bool validate() const override {
         return true;
     }
@@ -68,12 +101,19 @@ class bombOrder : public Order {
             executed = true;
         }
     }
+
+    std::string toString() const override {
+        return Order::toString(); // Use the base class implementation
+    }
 };
 
 class blockadeOrder : public Order {
   public:
+
+    blockadeOrder() {
+        name = "Blockade Order";
+    }
     bool validate() const override {
-        // For now, let's assume this order is always valid
         return true;
     }
 
@@ -83,10 +123,19 @@ class blockadeOrder : public Order {
             executed = true;
         }
     }
+
+    std::string toString() const override {
+        return Order::toString(); // Use the base class implementation
+    }
 };
 
 class airliftOrder : public Order {
     public:
+
+    airliftOrder() {
+        name="Airlift Order";
+    }
+
     bool validate() const override {
         return true;
     }
@@ -97,10 +146,19 @@ class airliftOrder : public Order {
             executed = true;
         }
     }
+
+    std::string toString() const override {
+        return Order::toString(); // Use the base class implementation
+    }
 };
 
 class negotiateOrder : public Order {
     public:
+
+    negotiateOrder() {
+        name = "Negotiate Order";
+    }
+
     bool validate() const override {
         return true;
     }
@@ -110,6 +168,10 @@ class negotiateOrder : public Order {
             effect = "Negotiate troops";
             executed = true;
         }
+    }
+
+    std::string toString() const override {
+        return Order::toString(); // Use the base class implementation
     }
 };
 
@@ -135,6 +197,12 @@ class orderList {
             newIndex >= 0 && newIndex < orders.size()) {
             std::swap(orders[oldIndex], orders[newIndex]);
             }
+    }
+
+    void showAllOrders() const {
+        for (const auto& order : orders) {
+            std::cout << order->toString(); // or you can just do std::cout << *order;
+        }
     }
 
     friend std::ostream& operator<<(std::ostream& os, const orderList& ordersList) {
