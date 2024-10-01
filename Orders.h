c98434e57e14894e3622bb97894b8a31d0cbb62f
mengge-he <125909363+mengge-he@users.noreply.github.com>
2024-09-30 21:32:47 -0400

    Update Orders.h

--- Orders.h
+++ Orders.h
@@ -4,11 +4,39 @@
 
 #ifndef ORDERS_H
 #define ORDERS_H
-
-
+#include <iostream>
+#include <string>
+#include <vector>
 
 class Orders {
+protected:
+    std::string* effect;
+    bool* executed;
 
+public:
+    std::string* name;
+
+    // Constructor
+    Orders();
+
+    // Destructor
+    virtual ~Orders();
+
+    // Copy constructor
+    Orders(const Orders& other);
+
+    // Assignment operator
+    Orders& operator=(const Orders& other);
+
+    // Virtual methods for validation and execution
+    virtual bool validate() const = 0;
+    virtual void execute() = 0;
+
+    // ToString method
+    virtual std::string toString() const;
+
+    // Stream insertion operator
+    friend std::ostream& operator<<(std::ostream& os, const Orders& order);
 };
 
 
