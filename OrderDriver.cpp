d810b57e61007f2951fba45a3e90f36048c66024
mengge-he <125909363+mengge-he@users.noreply.github.com>
2024-10-01 23:04:16 -0400

    Update OrderDriver.cpp

--- OrderDriver.cpp
+++ OrderDriver.cpp
@@ -1,6 +1,5 @@
-
 #include "OrderDriver.h"
-#include "Orders.cpp"
+#include "Orders.h"
 
 void testOrdersLists() {
 
@@ -19,16 +18,14 @@ void testOrdersLists() {
 
     // Execute some orders
     ordersList.removeOrder(0); // Remove first order (DeployOrder)
-    ordersList.moveOrder(2, 0); // Move Blockade order  to the first position
+    ordersList.moveOrder(2, 0); // Move Blockade order to the first position
 
     std::cout << "\n\nOrders after modifications (move and remove):\n";
     ordersList.showAllOrders();
 
-    // Clean up the orders list to avoid memory leaks
-    // Since orderList destructor now handles deletion, no need for manual cleanup
 }
 
 int main() {
-    test();
+    testOrdersLists();
     return 0;
 }
